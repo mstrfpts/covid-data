460ac63306aa36e5a0c83fb3f5cc7c9d1d42ad7b
derick-dsilva <mstrfpts@gmail.com>
2020-04-03 12:32:20 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Covid from "./Covid";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Covid />
+        asdf
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Covid.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import "./Covid.css";
+import _ from "lodash";
+
+export default class Covid extends Component {
+  constructor() {
+    super();
+    this.state = { data: [], DataReady: false };
+  }
+
+  componentDidMount() {
+    fetch(`https://corona.lmao.ninja/countries`)
+      .then(res => res.json())
+      .then(json => this.setState({ data: json }))
+      .then(this.setState({ DataReady: true }));
+  }
+
+  getTotalCases = () => {
+    let sumis = _.sumBy(this.state.data, el => {
+      return el.cases;
+    });
+    return sumis;
+  };
+
+  getActiveCases = () => {
+    let sumis = _.sumBy(this.state.data, el => {
+      return el.active;
+    });
+    return sumis;
+  };
+
+  getDeaths = () => {
+    let sumis = _.sumBy(this.state.data, el => {
+      return el.deaths;
+    });
+    return sumis;
+  };
+
+  render() {
+    console.log("state is ", this.state);
+    return (
+      <div>
+        <div className={"MajorCounts"}>
+          {this.state.DataReady ? (
+            <div>
+              CoVid-19
+              <div>{`Total Cases: ${this.getTotalCases()}`}</div>
+              <div>{`Active Cases: ${this.getActiveCases()}`}</div>
+              <div>{`Deaths: ${this.getDeaths()}`}</div>
+            </div>
+          ) : null}
+        </div>
+
+        <tr className={"TitleRow"}>
+          <th className={"TitleCol"}>Country</th>
+          <th className={"TitleCol"}>Case Count</th>
+          <th className={"TitleCol"}>Active Case Count</th>
+          <th className={"TitleCol"}>Deaths</th>
+        </tr>
+
+        {this.state.data.map(el => (
+          <tr className={"Row"} key={el.country}>
+            <td className={"TitleCol"}>{el.country}</td>
+            <td className={"TitleCol"}>{el.cases}</td>
+            <td className={"TitleCol"}>{el.active}</td>
+            <td className={"TitleCol"}>{el.deaths}</td>
+          </tr>
+        ))}
+      </div>
+    );
+  }
+}

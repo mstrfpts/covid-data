1c2a4e5441c035038e37c53830b92f5430935dee
derick-dsilva <mstrfpts@gmail.com>
2020-05-18 16:33:42 +0100

    Refactored code to have independent Table components

--- src/Covid.jsx
+++ src/Covid.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import "./Covid.css";
 import _ from "lodash";
-import TableRows from "./TableRows";
-import Chart from "./Chart";
-import formattedDate from "./Date";
+import Table from "./Components/Table";
+import Chart from "./Components/Chart";
+import formattedDate from "./Components/Date";
 
 import { initGA, PageView } from "./Tracking";
 
@@ -152,7 +152,7 @@ export default class Covid extends Component {
   updateCountryData = countryClicked => {
     let countriesDataArray = this.state.data;
     let selectedCountryData = countriesDataArray.find(
-      element => element.country == countryClicked
+      element => element.country === countryClicked
     );
 
     this.setState({
@@ -298,6 +298,13 @@ export default class Covid extends Component {
     const { filteredData, filterValue, graphData } = this.state;
     let graphDaysOptions = [1, 2, 3, 4, 5, 10, 20, 30, 40, 50, 60, 70];
     let graphDataOptions = ["Cases", "Deaths"];
+    let tableColumns = [
+      "Country",
+      "Cases(+new)",
+      "Active",
+      "Deaths(+new)",
+      "Tests"
+    ];
 
     return (
       <div className={"imageContainer"}>
@@ -424,33 +431,12 @@ export default class Covid extends Component {
               ) : null}
             </div>
 
-            <table id="Header" className={"Table"}>
-              <tbody>
-                <tr className={"TitleRow"}>
-                  <th className={"Col"}>Country</th>
-                  <th className={"Col"}>Cases(+new)</th>
-                  <th className={"Col"}>Active </th>
-                  <th className={"Col"}>Deaths(+new)</th>
-                  <th className={"Col"}>Tests</th>
-                </tr>
-              </tbody>
-
-              <div
-                style={{
-                  height: this.scrollHeightSetter()
-                }}
-                className={"Scroll"}
-              >
-                {filteredData.map((el, index) => (
-                  <TableRows
-                    key={index}
-                    row={el}
-                    index={index}
-                    updateGraph={this.updateGraph}
-                  />
-                ))}
-              </div>
-            </table>
+            <Table
+              columns={tableColumns}
+              heightSetter={this.scrollHeightSetter}
+              filteredData={filteredData}
+              updateGraph={this.updateGraph}
+            />
           </div>
         </div>
       </div>
